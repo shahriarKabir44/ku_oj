30377600b88d5661ee692861fbf6d840fc8b309d
shahriarKabir44 <shahriar1904@cseku.ac.bd>
2023-04-17 21:51:24 +0600

    updated navbar

--- src/shared/NavBar/DirectoryIndicator/DirectoryIndicator.jsx
+++ src/shared/NavBar/DirectoryIndicator/DirectoryIndicator.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import './DirectoryIndicator.css'
 function DirectoryIndicator(props) {
+    const dirs = [
+        {
+            label: 'home',
+            path: '/'
+        }, {
+            label: 'create contest',
+            path: '/createContest'
+        }
+    ]
     return (
         <div className='directoryIndicatorContainer'>
-
+            {dirs.map((dir, index) => {
+                return <div key={index} className="directoryBtnElement">
+                    <DirectoryButton dir={dir} />
+                    {index !== dirs.length - 1 && <p>/</p>}
+                </div>
+            })}
         </div>
     );
 }
 
 export default DirectoryIndicator;
+
+
+
+function DirectoryButton({ dir }) {
+    return <div className="directoryElementContainer">{dir.label} </div>
+}

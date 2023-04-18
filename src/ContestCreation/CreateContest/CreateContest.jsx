b31e8ff579f0cf933c0857cdd591f78619c67640
shahriarKabir44 <shahriar1904@cseku.ac.bd>
2023-04-18 12:51:18 +0600

    updated styles

--- src/ContestCreation/CreateContest/CreateContest.jsx
+++ src/ContestCreation/CreateContest/CreateContest.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import CreateProblem from '../CreateProblem/CreateProblem';
 import ContestService from '../../services/Contest.service';
 import EventSubscriptionManager from '../../EventsManager/EventSubscriptionManager'
+import DeleteIcon from '@mui/icons-material/Delete';
+import AddIcon from '@mui/icons-material/Add';
 import './CreateContest.css'
 import NavbarDirectoryManager from '../../EventsManager/NavbarDirectoryManager'
 function CreateContest(props) {
@@ -14,14 +16,14 @@ function CreateContest(props) {
         hostId: 1
     })
     React.useEffect(() => { }, [])
-    const [problemCount, setProblemCount] = React.useState([])
+    const [problemCount, setProblemCount] = React.useState([1, 2, 3, 4, 5, 6, 7, 8])
     return (
         <div className="container_createContest">
             <div className="dashboardContainer">
                 <div className="leftPanel">
                     <div className="contestDetailsPanel">
 
-                        <div className="card">
+                        <div className="card" style={{ height: "30vh" }}>
                             <h2 className="title">Create a contest</h2>
                             <div className='formContainer'>
                                 <label htmlFor="text">Text Input:</label>
@@ -39,6 +41,25 @@ function CreateContest(props) {
                                     setContestInfo({ ...contestInfo, endTime: e.target.value })
                                 }} value={contestInfo.endTime} type="datetime-local" name="trip-end" />
 
+                            </div>
+                        </div>
+                    </div>
+                    <div className="problemsLabelPanel">
+                        <div className="card">
+                            <div className="titleContainer">
+                                <h3 className="title">Problems</h3>
+                                <button className="addProblemBtn">
+                                    <AddIcon />
+                                </button>
+                            </div>
+                            <div className="problemsContainer">
+                                {problemCount.map((problem, index) => {
+                                    return <div key={index} className="problemItem">
+                                        <div className="problemLabel">Problem{problem}</div>
+                                        <div className="deleteBtn"> <DeleteIcon /></div>
+
+                                    </div>
+                                })}
                             </div>
                         </div>
                     </div>

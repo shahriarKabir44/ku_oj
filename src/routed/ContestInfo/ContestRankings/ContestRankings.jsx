b1ac952e0ecc610dbb549e6187e9c6bbed626f2d
adnangif <muhammadfahim010@gmail.com>
2023-07-02 17:10:32 +0600

    ranking design changes

--- src/routed/ContestInfo/ContestRankings/ContestRankings.jsx
+++ src/routed/ContestInfo/ContestRankings/ContestRankings.jsx
@@ -1,49 +1,99 @@
-import React from 'react'
-import ContestService from '../../../services/Contest.service'
-import './ContestRankings.css'
-export default function ContestRankings({ contestId, currentUser, problems }) {
-    const [pageNumber, setPageNumber] = React.useState(0)
-    const [rankings, setRankingList] = React.useState([])
-    const [isOfficial, toggleOfficialValue] = React.useState(true)
-    function gerContestStandings() {
-        console.log(contestId)
-        ContestService.getContestStandings(contestId, pageNumber, isOfficial)
-            .then(standings => {
-                standings.forEach((ranking) => {
-                    ranking.official_description = JSON.parse(ranking.official_description)
-                    ranking.description = JSON.parse(ranking.description)
-                    ranking.official_description = isOfficial ? ranking.official_description : ranking.description
-                    ranking.official_points = isOfficial ? ranking.official_points : ranking.points
-                })
-                let filteredStandingInfo = []
-                standings.forEach((ranking) => {
-                    if (isOfficial && ranking.official_description == null) return
-                    filteredStandingInfo.push(ranking)
-                });
-                console.log(filteredStandingInfo)
-                setRankingList(filteredStandingInfo)
-            })
-    }
-    React.useEffect(() => {
-        gerContestStandings()
-    }, [contestId, currentUser, isOfficial])
+import React from "react";
+import ContestService from "../../../services/Contest.service";
+import "./ContestRankings.css";
 
-    return (
-        <div className="contestStantingsContainer">
-            <label htmlFor="showOfficial">show official</label>
-            <input checked={isOfficial} type="checkbox" onChange={(e) => {
-                toggleOfficialValue(1 ^ isOfficial)
-            }} name="showOfficial" id="" />
-            <table style={{
-                width: 'auto',
-                borderCollapse: 'unset',
-                whiteSpace: 'nowrap'
-            }}>
+function RankingItem({ problems, rank }) {
+  return (
+    <div className="table-row">
+      <div className="participants-container">{rank.contestantName}</div>
+      <div className="problems-container">
+        {problems.map((problem, index) => {
+          let data = rank.official_description[problems.id];
+
+          if (data === 0) {
+            return <div className="not-submitted">N/A</div>;
+          } else if (data > 0) {
+            return <div className="passed-submit">Passed</div>;
+          } else {
+            return <div className="failed-submit">Failed</div>;
+          }
+        })}
+      </div>
+      <div className="points-container">{rank.official_points}</div>
+    </div>
+  );
+}
+
+export default function ContestRankings({ contestId, currentUser, problems }) {
+  const [pageNumber, setPageNumber] = React.useState(0);
+  const [rankings, setRankingList] = React.useState([]);
+  const [isOfficial, toggleOfficialValue] = React.useState(true);
+  function gerContestStandings() {
+    ContestService.getContestStandings(contestId, pageNumber, isOfficial).then(
+      (standings) => {
+        standings.forEach((ranking) => {
+          ranking.official_description = JSON.parse(
+            ranking.official_description
+          );
+          ranking.description = JSON.parse(ranking.description);
+          ranking.official_description = isOfficial
+            ? ranking.official_description
+            : ranking.description;
+          ranking.official_points = isOfficial
+            ? ranking.official_points
+            : ranking.points;
+        });
+        let filteredStandingInfo = [];
+        console.log(standings);
+        standings.forEach((ranking) => {
+          if (isOfficial && ranking.official_description == null) return;
+          filteredStandingInfo.push(ranking);
+        });
+        // console.log(filteredStandingInfo)
+        setRankingList(filteredStandingInfo);
+      }
+    );
+  }
+  React.useEffect(() => {
+    gerContestStandings();
+  }, [contestId, currentUser, isOfficial]);
+
+  return (
+    <div className="contestStantingsContainer">
+      <label htmlFor="showOfficial">show official</label>
+      <input
+        checked={isOfficial}
+        type="checkbox"
+        onChange={(e) => {
+          toggleOfficialValue(1 ^ isOfficial);
+        }}
+        name="showOfficial"
+        id=""
+      />
+
+      <div className="main-container">
+        <div className="table-row">
+            <div className="participants-container">Participants</div>
+            <div className="problems-container">
+                {
+                    problems.map((_,index)=>(
+                        <div className="problem-title"> {String.fromCharCode(65+index)} </div>
+                    ))
+                }
+
+            </div>
+            <div className="points-container">points</div>
+
+        </div>
+        {rankings.map((rank, index) => (
+          <RankingItem key={index} problems={problems} rank={rank} />
+        ))}
+      </div>
+      {/* <table>
                 <thead>
                     <tr>
                         <th>
-                            <p>Participant</p>
-
+                            <p>Participants</p>
                         </th>
                         <th>Points</th>
                         {problems.map((problem, index) => {
@@ -70,7 +120,7 @@ export default function ContestRankings({ contestId, currentUser, problems }) {
                     })}
 
                 </tbody>
-            </table>
-        </div>
-    )
+            </table> */}
+    </div>
+  );
 }

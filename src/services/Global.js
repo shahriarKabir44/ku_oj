7d1e9b4e0ea59b717e4227b3d8d1c5847e4be37b
shahriarKabir44 <shahriar1904@cseku.ac.bd>
2023-10-20 20:10:07 +0600

    added gh pages

--- src/services/Global.js
+++ src/services/Global.js
@@ -2,10 +2,10 @@ export default class Global {
     // static SERVER_IP = "http://192.168.0.105"
     //"https://4ffa-103-25-251-250.ngrok-free.app" //// 
 
-    static SERVER_URL = "http://localhost:8080"// "https://kuoj.onrender.com"
+    static SERVER_URL = "https://kuoj.onrender.com"
 
-    static WS_URL = "ws://localhost:8080"
-    static CLIENT_URL = 'http://localhost:3000'
+    static WS_URL = "ws://kuoj.onrender.com"
+    static CLIENT_URL = 'https://shahriarkabir44.github.io/ku_oj/'
     // static SERVER_IP = 
     static async _fetch(url, body = null) {
         let payload = {
